6c8faeb62f22709941b07f37f219fa5a7ac2f266
ELENA ZVEREVA <55851043+lenazve1996@users.noreply.github.com>
2022-12-24 21:46:19 +0300

    Update install_deps.sh
    fix volume dir on host

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /home/ayajirob/mariadb
-mkdir -p /home/ayajirob/wordpress
+mkdir -p /home/ayajirob/data/mariadb
+mkdir -p /home/ayajirob/data/wordpress
 #chmod g+w volumes/mariadb
 #chmod g+w volumes/wordpress
